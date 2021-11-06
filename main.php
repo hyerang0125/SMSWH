48d6c09fac824c0abf42dc09a2dcfe99430f8200
mayo_ <59410565+hyerang0125@users.noreply.github.com>
2021-11-06 21:55:45 +0900

    proto1

---
+++ main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+<html>
+    <head>
+        <title>내가 숙명 지박령이었다면?</title>
+        <meta name="viewport" content="width=device-width, inital-scale=1.0"/>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="wrap">
+            <span class="mainTitle">내가 숙명 지방력이었다면?</span>
+            <img src=".">
+            <button class="startBtn" type="button">테스트 시작</button>
+        </div>
+    </body>
+</html>
